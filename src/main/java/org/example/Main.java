a05c2b58241695a4cb6d1fefa17bb621ab1342d6
Nikita_Hubn <nvp33xzc@yandex.ru>
2025-07-04 12:32:37 +0300

    04.07.25

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // (2) Группированы заказы по продуктам и (3) Для каждого продукта найдена общая стоимость всех заказов.
+        Map<String, Double> orderGroupByProductAndTotalSumProduct = orders.stream().collect(Collectors.groupingBy(Order::getProduct,
+                Collectors.summingDouble(Order::getCost)));
+
+        // (4) Отсортируем продукты по убыванию общей стоимости и (5) Выбираем топ-3
+        List<Map.Entry<String, Double>> sortedByDescProductAndTop3Products = orderGroupByProductAndTotalSumProduct.entrySet().stream()
+                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+
+        // 6. Выводим результат трех самых дорогих продуктов и их общая стоимость.
+        System.out.println(sortedByDescProductAndTop3Products);
     }
 }
--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Order {
+    private String product;
+    private double cost;
+
+    public Order(String product, double cost) {
+        this.product = product;
+        this.cost = cost;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String toString() {
+        return "Order(product=" + this.getProduct() + ", cost=" + this.getCost() + ")";
+    }
+
 }
